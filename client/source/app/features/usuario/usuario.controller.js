51711ff935c5a58835a98a724ebb8e100c48d5df
johnkaick <johnkaick@gmail.com>
2017-07-03 23:57:52 -0300

    implantando services api

--- client/source/app/features/usuario/usuario.controller.js
+++ client/source/app/features/usuario/usuario.controller.js
@@ -5,7 +5,24 @@
         .module('app')
         .controller('usuarioCtrl', usuarioCtrl)
 
-    function usuarioCtrl($scope) {
+    function usuarioCtrl($scope, $uibModal, usuarioAPI) {
+
+        $scope.carregaLista = function () {
+            usuarioAPI.obterLista().then(function (result) {
+                $scope.listas = result.data
+            }).catch(function (data) {
+                $scope.message = 'Error: ' + data
+            })
+        }
+
+        $scope.cadastrar = function () {
+            $uibModal.open({
+                templateUrl: '/features/usuario/usuario.cadastro.html',
+                controller: 'usuarioCtrl',
+            }).result.then(function () {
+                $scope.carregaLista()
+            })
+        }
     }
 
 }());
